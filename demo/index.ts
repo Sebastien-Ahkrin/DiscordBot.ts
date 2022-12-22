4a56d2995ed2f544758235dc00e64296deaa5730
Sebastien-Ahkrin <sebastien48criquet@gmail.com>
2022-12-22 16:46:43 +0100

    chore: add command hanlder

--- demo/index.ts
+++ demo/index.ts
@@ -1,3 +1,16 @@
 import { DiscordClient } from './../src/index';
 
 const client = new DiscordClient({ intents: 'DiscordClientAllIntents' });
+
+client.addCommand(
+  {
+    name: 'test',
+    description: 'Vous affiche le profils du joueur',
+    options: [{ name: 'user', type: 6, description: 'User', required: true }],
+  },
+  (interaction) => {
+    interaction.reply('Hello, World!');
+  },
+);
+
+client.login('SET A TOKEN');
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import {
+  ApplicationCommandDataResolvable,
   BitFieldResolvable,
   Client,
   ClientOptions,
   GatewayIntentsString,
+  Events,
+  Interaction,
+  Collection,
+  ChatInputCommandInteraction,
 } from 'discord.js';
 
 enum DiscordGatewayIntentsString {
@@ -15,7 +20,14 @@ interface DiscordClientOptions extends Omit<ClientOptions, 'intents'> {
     | BitFieldResolvable<keyof typeof DiscordGatewayIntentsString, number>;
 }
 
+type CommandAction = (interaction: ChatInputCommandInteraction) => void;
+
 export class DiscordClient extends Client {
+  private commands: Collection<
+    ApplicationCommandDataResolvable,
+    CommandAction
+  > = new Collection();
+
   constructor(options: DiscordClientOptions) {
     if (options.intents !== 'DiscordClientAllIntents') {
       super(options as ClientOptions);
@@ -25,5 +37,35 @@ export class DiscordClient extends Client {
       ...options,
       intents: DiscordGatewayIntentsString['DiscordClientAllIntents'],
     });
+
+    this.on(Events.InteractionCreate, this.handleCommands);
+    this.on(Events.ClientReady, this.registerCommands);
+  }
+
+  /**
+   * Register each commands to discord
+   */
+  private registerCommands() {
+    for (const [command] of this.commands) {
+      this.application?.commands.create(command);
+    }
+  }
+
+  public addCommand(
+    command: ApplicationCommandDataResolvable,
+    callback: CommandAction,
+  ) {
+    this.commands.set(command, callback);
+  }
+
+  public handleCommands(interaction: Interaction) {
+    if (!interaction.isChatInputCommand()) return;
+    const { commandName } = interaction;
+
+    const command = this.commands.find(
+      (_, key: any) => key.name === commandName,
+    );
+
+    command?.(interaction);
   }
 }
